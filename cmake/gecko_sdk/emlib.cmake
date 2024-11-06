2871a8638c428d2296977d2860e2684aa2ce821b
James Smith <james@loopj.com>
2024-11-06 10:27:37 -0800

    Flip emlib back to object library

--- cmake/gecko_sdk/emlib.cmake
+++ cmake/gecko_sdk/emlib.cmake
@@ -1,6 +1,6 @@
 # CMake target for Gecko EMLIB
 if(NOT TARGET GeckoSDK_emlib)
-  add_library(GeckoSDK_emlib STATIC)
+  add_library(GeckoSDK_emlib OBJECT)
   add_library(GeckoSDK::emlib ALIAS GeckoSDK_emlib)
 
   # Common include paths
